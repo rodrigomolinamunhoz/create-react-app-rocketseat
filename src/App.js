a20980c57a997cc03a40518a69e3420d989f92bb
Rodrigo <rodrigo.molina@labsit.io>
2019-07-04 10:34:19 -0300

    Aula 9 - configurando navegação

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Routes from './routes';
 import './styles.css';
 import Header from './components/Header';
 import Main from './pages/main';
@@ -6,7 +7,7 @@ import Main from './pages/main';
 const App = () => (
   <div className="App">
     <Header />
-    <Main />
+    <Routes />
 </div>
 );
 
