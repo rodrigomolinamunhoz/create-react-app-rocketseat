0ac75a466fe51e2b38fa464a4aeefedcf6ffecc0
Rodrigo <rodrigo.molina@labsit.io>
2019-07-04 08:54:43 -0300

    Aula 4 - Criando o Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
+import './styles.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello Rocketseat</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+</div>
+);
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => <header id="main-header">JRHunt</header>;
+
+export default Header;
